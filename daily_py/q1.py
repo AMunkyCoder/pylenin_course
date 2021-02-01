d6433e34481f1b155a8d443588bf7dc673b34c32
drMunkCoder <77227551+drMunkCoder@users.noreply.github.com>
2021-02-02 06:40:57 +0800

    Added Questions

--- daily_py/q1.py
+++ daily_py/q1.py
@@ -1,3 +1,6 @@
+# Qns 1
+# Print My Name Is Pylenin as My|Name|Is|Pylenin using output formatting of a print() function.
+
 my_list = ["My", "Name", "Is", "Pylenin"]
 
 for let in my_list:
--- daily_py/q2.py
+++ daily_py/q2.py
@@ -1,3 +1,12 @@
+# Qns 2
+# Improve the following code so that it prints over the same line.
+# print("Welcome")
+# print("to")
+# print("Python")
+# print("Bootcamp")
+# print("2021")
+
+
 print("Welcome", end=" ")
 print("to",  end=" ")
 print("Python",  end=" ")
