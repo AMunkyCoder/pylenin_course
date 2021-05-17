5194051b60c506a01e8f03ec8ca294ea95ebd65f
drMunkCoder <77227551+drMunkCoder@users.noreply.github.com>
2021-05-18 07:46:52 +0800

    Edited spacing

--- daily_py/day_11/q1.py
+++ daily_py/day_11/q1.py
@@ -1,2 +1,2 @@
-# Write a Python program to take values of length and breadth of a rectangle from user and check if it is square or not.
-
+# Write a Python program to take values of length and breadth of a 
+# rectangle from user and check if it is square or not.
